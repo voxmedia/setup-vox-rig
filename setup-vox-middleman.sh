928447e07107dc595b3146d53d56213e5ba31e1c
Ryan Mark <ryan@mrk.cc>
2016-12-02 09:54:46 -0500

    prompt help

--- setup-vox-middleman.sh
+++ setup-vox-middleman.sh
@@ -259,7 +259,7 @@ if [ "$INSTALL_CHORUS" = true ] ; then
   fi
 
   if [ -z "$CHORUS_API_APPLICATION_ID" ] ; then
-    echo 'Setting up your Chorus account...'
+    echo 'Setting up your Chorus account... (ask for this info in #growthdev-tools)'
     read -p 'Application ID: ' chorus_id
     read -p 'Application Secret: ' chorus_secret
     echo "export CHORUS_API_APPLICATION_ID=$chorus_id" >> ~/.bash_profile
